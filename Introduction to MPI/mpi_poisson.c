6cc8230edb100883362d1821de116d67d6bcfb5d
rportocarrero <portocarrero.ryan@gmail.com>
2022-08-26 10:39:35 -0400

    Update mpi_poisson.c
    Parallelized test

--- Introduction to MPI/mpi_poisson.c
+++ Introduction to MPI/mpi_poisson.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <mpi.h>
 
 #define GRIDSIZE 10
 
@@ -28,22 +29,34 @@ double poisson_step(float *u, float *unew, float *rho, float hsq, int points){
 int main(int argc, char** argv){
     
     int i, j;
-    float u[GRIDSIZE+2], unew[GRIDSIZE+2], rho[GRIDSIZE+2];
+    float *u, *unew, *rho;
     float h, hsq;
-    double unorm;
+    double unorm, residual;
+    int n_ranks, rank, my_j_max;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &n_ranks);
+    my_j_max = GRIDSIZE/n_ranks;
+
+    u = malloc((my_j_max+2) * sizeof(float*));
+    unew = malloc((my_j_max+2) * sizeof(float*));
+    rho = malloc((my_j_max+2) * sizeof(float*));
 
     h = 0.1;
 
-    hsq = h*h;
+    hsq = h * h;
 
-    for(int i=0; i<=GRIDSIZE+1;i++){
+    for(int i=0; i<=my_j_max+1; i++){
         u[i] = 0.0;
         rho[i] = 0.0;
     }
 
-    u[0] = 10;
+    if(rank == 0)
+        u[0] = 10;
 
-    unorm = poisson_step(u, unew, rho, hsq, GRIDSIZE);
+    unorm = poisson_step(u, unew, rho, hsq, my_j_max);
 
     if(unorm == 25){
         printf("TEST SUCCEEDED after 1 iteration\n");
@@ -52,14 +65,20 @@ int main(int argc, char** argv){
         printf("Norm %g\n", unorm);
     }
 
-    for(i =1; i<10; i++){
-        unorm = poisson_step(u, unew, rho, hsq, GRIDSIZE);
+    for( i=1; i<=0; i++){
+        unorm = poisson_step(u, unew, rho, hsq, my_j_max);
     }
 
-    if( fabs(unorm- 0.463676) < 1e-6){
+    if(fabs(unorm - 0.463676) < 1e-6){
         printf("TEST SUCCEEDED after 10 iterations\n");
     } else {
         printf("TEST FAILED after 10 iterations\n");
         printf("Norm %g\n", unorm);
     }
+
+    free(u);
+    free(unew);
+    free(rho);
+
+    return MPI_Finalize();
 }
